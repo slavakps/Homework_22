4f373bbd7e9a4e69cb77c9c3fbb3d04643ef19e3
Your Name <you@example.com>
2025-10-11 14:41:04 +0800

    создал новое приложение для блога

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,16 +1,15 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, product_detail, ProductListView
+from catalog.views import ProductListView, ProductDetailView, ContactsTemplateView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
-    path('product/<int:product_id>/', product_detail, name='product_detail'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
 
 if settings.DEBUG:
